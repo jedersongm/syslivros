eb8842dea2b2359995ac267e9ac310b8c9c0a004
Jéderson Gonçalves <jedersongmoura@gmail.com>
2016-01-24 03:22:28 -0300

    Entidades

---
+++ src/main/java/Entidades/Comentario.java
@@ -0,0 +1,46 @@
+
+package Entidades;
+
+import java.sql.Timestamp;
+
+public class Comentario {
+  private int id;
+  private String texto;
+  private Timestamp data;
+  private Usuario usuario;
+  private Topico topico;
+  
+ public int getId() {
+ 	return id;
+ }
+ public void setId(int id) {
+ 	this.id = id;
+ }
+ public String getTexto() {
+ 	return texto;
+ }
+ public void setTexto(String texto) {
+ 	this.texto = texto;
+ }
+ public Timestamp getData() {
+ 	return data;
+ }
+ public void setData(Timestamp data) {
+ 	this.data = data;
+ }
+ public Usuario getUsuario() {
+ 	return usuario;
+ }
+ public void setUsuario(Usuario usuario) {
+ 	this.usuario = usuario;
+ }
+ public Topico getTopico() {
+ 	return topico;
+ }
+ public void setTopico(Topico topico) {
+ 	this.topico = topico;
+ }
+ 
+  
+}
+
---
+++ src/main/java/Entidades/Foto.java
@@ -0,0 +1,30 @@
+package Entidades;
+
+import java.sql.Timestamp;
+
+public class Foto {
+  private String url;
+  private Timestamp data;
+  private Usuario usuario;
+  
+ public String getUrl() {
+ 	return url;
+ }
+ public void setUrl(String url) {
+ 	this.url = url;
+ }
+ public Timestamp getData() {
+ 	return data;
+ }
+ public void setData(Timestamp data) {
+ 	this.data = data;
+ }
+ public Usuario getUsuario() {
+ 	return usuario;
+ }
+ public void setUsuario(Usuario usuario) {
+ 	this.usuario = usuario;
+ }
+ 
+  
+}
